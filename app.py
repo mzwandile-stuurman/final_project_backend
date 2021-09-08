0e9433b6c447bdbb0c346a3853af4f8638edf1ba
mzwandile-stuurman <stuurmanmzwandile@gmail.com>
2021-09-08 16:24:32 +0200

    push to home

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from smtplib import SMTPRecipientsRefused, SMTPAuthenticationError
 from werkzeug.utils import redirect
 
 app = Flask(__name__)
-CORS(app, resoures={r"/api/*/": {"origins": "*"}})
+CORS(app, resoures={r"/api/*": {"origins": "*"}})
 app.debug = True
 app.config['SECRET_KEY'] = 'super-secret'
 app.config['JWT_EXPIRATION_DELTA'] = datetime.timedelta(days=2)
